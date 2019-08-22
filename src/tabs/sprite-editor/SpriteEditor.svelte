784ec42c04be0e123dfa3caae3b02740784e8639
Jacob <jacob.romano@metry.io>
2019-08-22 20:40:04 +0200

    added draw and erase pen modes and refactored slots into component

--- src/tabs/sprite-editor/SpriteEditor.svelte
+++ src/tabs/sprite-editor/SpriteEditor.svelte
@@ -1,6 +1,7 @@
 <script>
     import {onMount} from 'svelte';
     import {sprites} from "../../stores/sprites";
+    import SpriteSlots from './SpriteSlots.svelte';
 
     let spriteEditorCanvas;
     let spriteEditorContext;
@@ -10,83 +11,102 @@
     let spritesStoreUnsubscribe = sprites.subscribe((sprite) => {
         spriteStore = sprite;
     });
+    let isMouseDown = false;
+    const penModes = {
+        draw: 0,
+        erase: 1,
+    };
+    let activePenMode = penModes.draw;
 
     onMount(() => {
         spriteEditorCanvas = document.querySelector('#sprite-editor-canvas');
         pixelsPerUnit = spriteEditorCanvas.width / spriteEditorResolution;
         spriteEditorCanvas.addEventListener('click', (clickEvent) => {
-            const {canvasX, canvasY} = spriteEditor.getCanvasCoordinates(clickEvent);
-            const index = spriteEditor.canvasCoordinatesToPixelIndex(canvasX, canvasY);
-            spriteEditor.setPixelColor(index);
-            spriteEditorContext.fillStyle = spriteStore.slots[spriteStore.selectedSlot].pixels[index];
-            const {x, y} = spriteEditor.pixelIndexToCanvasCoordinates(index);
-            spriteEditorContext.fillRect(x, y, pixelsPerUnit, pixelsPerUnit)
+            handleCanvasMouseDown(clickEvent);
         });
         spriteEditorCanvas.addEventListener('mousedown', (event) => {
-            spriteEditor.isMouseDown = true;
+            isMouseDown = true;
         });
         spriteEditorCanvas.addEventListener('mousemove', (event) => {
-            if (spriteEditor.isMouseDown) {
-                const {canvasX, canvasY} = spriteEditor.getCanvasCoordinates(event);
-                const index = spriteEditor.canvasCoordinatesToPixelIndex(canvasX, canvasY);
-                spriteEditor.setPixelColor(index);
-                spriteEditorContext.fillStyle = spriteStore.slots[spriteStore.selectedSlot].pixels[index];
-                const {x, y} = spriteEditor.pixelIndexToCanvasCoordinates(index);
-                spriteEditorContext.fillRect(x, y, pixelsPerUnit, pixelsPerUnit)
+            if (isMouseDown) {
+                handleCanvasMouseDown(event);
             }
         });
         spriteEditorCanvas.addEventListener('mouseup', (event) => {
-            spriteEditor.isMouseDown = false;
+            isMouseDown = false;
         });
         spriteEditorContext = spriteEditorCanvas.getContext('2d');
-        spriteEditor.paintAll();
+        paintAll();
     });
 
-    let spriteEditor = {
-        clear: () => {
-            spriteEditorContext.fillStyle = 'white';
-            spriteEditorContext.fillRect(0, 0, spriteEditorCanvas.width, spriteEditorCanvas.height);
-        },
-        paintAll: () => {
-            spriteStore.slots[spriteStore.selectedSlot].pixels.forEach((color, index) => {
-                const {x, y} = {
-                    x: (index % spriteEditorResolution) * pixelsPerUnit,
-                    y: (Math.floor(index / spriteEditorResolution) % spriteEditorResolution) * pixelsPerUnit,
-                };
-                spriteEditorContext.fillStyle = color;
-                spriteEditorContext.fillRect(x, y, pixelsPerUnit, pixelsPerUnit);
-            })
-        },
-        canvasCoordinatesToPixelIndex: (x, y) => {
-            return Math.floor(x / pixelsPerUnit) + Math.floor(y / pixelsPerUnit) * Math.floor(spriteEditorCanvas.width / pixelsPerUnit);
-        },
-        pixelIndexToCanvasCoordinates: (index) => {
-            return {
+    function handleCanvasMouseDown(event) {
+        const {canvasX, canvasY} = getCanvasCoordinates(event);
+        const index = canvasCoordinatesToPixelIndex(canvasX, canvasY);
+
+        if (activePenMode === penModes.draw) {
+            setPixelColor(index, 'blue');
+            spriteEditorContext.fillStyle = spriteStore.slots[spriteStore.selectedSlot].pixels[index];
+            const {x, y} = pixelIndexToCanvasCoordinates(index);
+            spriteEditorContext.fillRect(x, y, pixelsPerUnit, pixelsPerUnit)
+        } else if (activePenMode === penModes.erase) {
+            setPixelColor(index, 'white');
+            spriteEditorContext.fillStyle = spriteStore.slots[spriteStore.selectedSlot].pixels[index];
+            const {x, y} = pixelIndexToCanvasCoordinates(index);
+            spriteEditorContext.fillRect(x, y, pixelsPerUnit, pixelsPerUnit)
+        }
+    }
+    
+    function clear() {
+        spriteEditorContext.fillStyle = 'white';
+        spriteEditorContext.fillRect(0, 0, spriteEditorCanvas.width, spriteEditorCanvas.height);
+    }
+
+    function drawRect(x, y, color) {
+        spriteEditorContext.fillStyle = color;
+        spriteEditorContext.fillRect(x, y, pixelsPerUnit, pixelsPerUnit);
+    }
+
+    function paintAll() {
+        spriteStore.slots[spriteStore.selectedSlot].pixels.forEach((color, index) => {
+            const {x, y} = {
                 x: (index % spriteEditorResolution) * pixelsPerUnit,
                 y: (Math.floor(index / spriteEditorResolution) % spriteEditorResolution) * pixelsPerUnit,
-            }
-        },
-        setPixelColor: (pixelIndex) => {
-            sprites.update(sprite => {
-                sprite.slots[sprite.selectedSlot].pixels[pixelIndex] = 'blue';
-                return sprite;
-            });
-        },
-        getCanvasCoordinates: (e) => {
-            const rect = spriteEditorCanvas.getBoundingClientRect();
-            const canvasX = Math.floor(e.clientX - rect.left);
-            const canvasY = Math.floor(e.clientY - rect.top);
-            return {canvasX, canvasY};
-        },
-        isMouseDown: false
-    };
-
-    function onSlotClicked(slotN) {
-        sprites.update(_sprites => {
-            _sprites.selectedSlot = slotN;
-            return _sprites;
+            };
+            drawRect(x, y, color);
+        })
+    }
+    
+    function canvasCoordinatesToPixelIndex(x, y) {
+        return Math.floor(x / pixelsPerUnit) + Math.floor(y / pixelsPerUnit) * Math.floor(spriteEditorCanvas.width / pixelsPerUnit);
+    }
+    
+    function pixelIndexToCanvasCoordinates(index) {
+        return {
+            x: (index % spriteEditorResolution) * pixelsPerUnit,
+            y: (Math.floor(index / spriteEditorResolution) % spriteEditorResolution) * pixelsPerUnit,
+        }
+    }
+    
+    function setPixelColor(pixelIndex, color) {
+        sprites.update(sprite => {
+            sprite.slots[sprite.selectedSlot].pixels[pixelIndex] = color;
+            return sprite;
         });
-        spriteEditor.paintAll();
+    }
+    
+    function getCanvasCoordinates(e) {
+        const rect = spriteEditorCanvas.getBoundingClientRect();
+        const canvasX = Math.floor(e.clientX - rect.left);
+        const canvasY = Math.floor(e.clientY - rect.top);
+        return {canvasX, canvasY};
+    }
+
+    function setPenMode(mode) {
+        activePenMode = penModes[mode];
+    }
+
+    function onSlotClicked() {
+        paintAll();
     }
 
 </script>
@@ -103,18 +123,17 @@
         align-self: start;
     }
 
-    .sprite-slots-list {
+    .editor-tools {
         display: flex;
         flex-direction: column;
-        flex-wrap: wrap;
     }
 </style>
 
 <div class="editor-container">
     <canvas width="512" height="512" id="sprite-editor-canvas"></canvas>
-    <div class="sprite-slots-list">
-        {#each spriteStore.slots as slot, i}
-        <button on:click={()=>onSlotClicked(i)}>{i}</button>
-        {/each}
+    <div class="editor-tools">
+        <button on:click={()=>setPenMode('draw')}>Draw</button>
+        <button on:click={()=>setPenMode('erase')}>Erase</button>
     </div>
+    <SpriteSlots on:slotClicked={onSlotClicked}/>
 </div>
---
+++ src/tabs/sprite-editor/SpriteSlots.svelte
@@ -0,0 +1,35 @@
+
+<script>
+    import { sprites } from "../../stores/sprites";
+    import {createEventDispatcher} from 'svelte';
+
+    let spriteStore;
+
+    const dispatch = createEventDispatcher();
+
+    const unsubscribeSpritesStore = sprites.subscribe((sprite) => {
+        spriteStore = sprite;
+    });
+
+    function onSlotClicked(slotN) {
+        sprites.update(_sprites => {
+            _sprites.selectedSlot = slotN;
+            return _sprites;
+        });
+        dispatch('slotClicked', {});
+    }
+</script>
+
+<style>
+    .sprite-slots-list {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
+</style>
+
+<div class="sprite-slots-list">
+    {#each spriteStore.slots as slot, i}
+    <button on:click={()=>onSlotClicked(i)}>{i}</button>
+    {/each}
+</div>
